1a3b4d9f55961b3e0533f06b1442fe1916f06783
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-04-04 23:28:28 +0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,16 @@
-let username = "umudvar"
-const DOMAIN = "khalisli"
-
-let email = username + @ + DOMAIN  
-console.log("username  ")
+let price = 111
+let stringprice = "111"
+let hasPassword = true
+console.log(
+        "price: ",
+        typeof(price)
+)
+console.log(
+        "stringprice = "111"
+        : ",
+        typeof(stringprice)
+)
+console.log(
+        "hasPassword: ",
+        typeof(hasPassword)
+)
