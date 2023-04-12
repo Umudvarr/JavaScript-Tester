55c227c5e58fde04f7b76a968586c3958a3840e1
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-04-13 00:29:56 +0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -18,3 +18,26 @@ switch(hava) {
   default:
       document.getElementById("wether").innerHTML = "Bilinmeyen hava durumu:";
 }
+//*************ikinci proje*************//
+
+ let islem = function(a, b, operator) {
+    switch(operator) {
+        case 'topla':
+            return a + b;
+        break;
+        case 'cikar':
+            return a - b;
+        break;
+        case 'carp':
+            return a * b;
+        break;
+        case 'bol':
+            return a / b;
+        break;
+        default:
+            return 'Tanimlanmamis islem';
+        break;
+    }
+};
+console.log(islem(23, 14, 'topla'));  // 37
+console.log(islem(10, 3, 'mod'));     // Tanimlanmamis islem
