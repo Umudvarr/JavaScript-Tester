62714ce1644ad590f460f4657f03c61eea116209
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-04-22 17:17:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,53 @@ console.log(document);
 // value = document.hr;
 
 console.log(value);
+
+
+// bunlar yeni elave olundu
+
+// let value;
+
+// const button = document.getElementById("todoAddButton");
+
+
+
+// console.log(value);
+// console.log(button.id);
+// console.log(button.className);
+// const classList = button.classList[3];
+// const classList = button.classList[2];
+
+// console.log(classList);
+
+// const buttonText = button.textContent;
+
+// console.log(buttonText);
+
+
+// const todoList = document.getElementsByClassName("list-group");
+
+// console.log(todoList);
+
+// const forms = document.getElementsByTagName("form");
+
+// console.log(forms[1].id);
+
+// const forms = document.getElementsByTagName("li");
+
+// console.log(forms)  ;
+
+
+// const clearButton = document.querySelector("#clearButton");
+
+// console.log(clearButton);
+
+// const todoList = document.querySelector(".list-group");
+
+// console.log(todoList);
+
+// const todoList = document.querySelector("li:nth-child(2)");
+
+// console.log(todoList);
+
+//************ */ odd && even tek ve cut
+
