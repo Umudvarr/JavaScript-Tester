54bb8867929aba6e92e4e6ee84d11fc81cf2a05b
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-04-30 17:42:10 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
 console.log(document);
 
-// value = document.location.links[4];
-// value = document.location;
-// value = document.hr;
+ value = document.location.links[4];
+ value = document.location;
+ value = document.hr;
 
 console.log(value);
 
 
 // bunlar yeni elave olundu
 
-// let value;
+let value;
 
-// const button = document.getElementById("todoAddButton");
-
-
-
-// console.log(value);
-// console.log(button.id);
-// console.log(button.className);
-// const classList = button.classList[3];
-// const classList = button.classList[2];
-
-// console.log(classList);
-
-// const buttonText = button.textContent;
-
-// console.log(buttonText);
+const button = document.getElementById("todoAddButton");
 
 
-// const todoList = document.getElementsByClassName("list-group");
 
-// console.log(todoList);
+console.log(value);
+console.log(button.id);
+console.log(button.className);
+const classList = button.classList[3];
+const classList = button.classList[2];
 
-// const forms = document.getElementsByTagName("form");
+console.log(classList);
 
-// console.log(forms[1].id);
+const buttonText = button.textContent;
 
-// const forms = document.getElementsByTagName("li");
-
-// console.log(forms)  ;
+console.log(buttonText);
 
 
-// const clearButton = document.querySelector("#clearButton");
+const todoList = document.getElementsByClassName("list-group");
 
-// console.log(clearButton);
+console.log(todoList);
 
-// const todoList = document.querySelector(".list-group");
+const forms = document.getElementsByTagName("form");
 
-// console.log(todoList);
+console.log(forms[1].id);
 
-// const todoList = document.querySelector("li:nth-child(2)");
+const forms = document.getElementsByTagName("li");
 
-// console.log(todoList);
+console.log(forms)  ;
+
+
+const clearButton = document.querySelector("#clearButton");
+
+console.log(clearButton);
+
+const todoList = document.querySelector(".list-group");
+
+console.log(todoList);
+
+const todoList = document.querySelector("li:nth-child(2)");
+
+console.log(todoList);
 
 //************ */ odd && even tek ve cut
 
