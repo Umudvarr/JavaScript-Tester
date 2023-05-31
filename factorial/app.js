6167dc8c8b33d058b8b5c8dc2e61a604c75716e4
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-06-01 00:15:24 +0400

    Update app.js

--- factorial/app.js
+++ factorial/app.js
@@ -1,6 +1,7 @@
 let number = Number(prompt("Plz enter a Number : "));
 let calculate = 1;
-for(let i = 1; i <= number; i ++){
+for(let i = 1; i <= number; i ++)
+{
     calculate = calculate*i;
 }
 alert("you are calculating number of factorial which you write : " + calculate);
