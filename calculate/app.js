c6e3342ae087dae3e2c9991164b00f22c3d4aaca
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-06-01 00:14:37 +0400

    Update app.js

--- calculate/app.js
+++ calculate/app.js
@@ -2,5 +2,5 @@ for(let i = 1; i <=10; i++){
     for(j=1; j <=10; j++){
         console.log(i+ "x"+ j+ "=" +(i*j));
     }
-    console.log("-------------------");
+    console.log("----------------------");
 }
