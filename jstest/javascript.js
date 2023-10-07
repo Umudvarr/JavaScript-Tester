79a0578955ca6746899ed7a69082f0abc54accf5
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-10-08 00:45:15 +0400

    Add files via upload

---
+++ jstest/javascript.js
@@ -0,0 +1,11 @@
+var info = [];
+info[0] = "Umudvar";
+info[1] = "Khalisli";
+info[2] = 25;
+info[3] = "Male";
+info[4] = "Frontend Developer";
+
+for (var i = 0; i < info.length; i++) {
+    document.write(info[i]);
+    document.write("<br>");
+}
